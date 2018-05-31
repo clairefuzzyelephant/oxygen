a3ba02cf937d8b52f0438aa3954ee7f19d3b350c
Claire Cheng <clairecheng88@gmail.com>
2018-05-30 19:48:01 -0700

    maze

--- maze.java
+++ maze.java
@@ -1,13 +1,38 @@
 import javax.swing.*;
 import java.awt.*;
 import java.util.*;
+import java.awt.geom.*;
 
 
 public class maze extends JPanel{
 
+  private Image livetree;
+
   public maze(){
 
+    initMaze();
 
   }
+
+
+  private void initMaze(){
+
+
+      loadImage();
+
+      int width = livetree.getWidth(this);
+      int height = livetree.getHeight(this);
+      setPreferredSize(new Dimension(width, height));
+    }
+
+    private void loadImage(){
+      ImageIcon ii = new ImageIcon("greentree.png");
+      livetree = ii.getImage();
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+      g.drawImage(livetree, 0, 0, null);
+    }
 
 }
