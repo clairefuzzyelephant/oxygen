22bb2c71b5ed6a0a6e5b7b6d44f1755253465a8a
Claire Cheng <clairecheng88@gmail.com>
2018-06-01 16:48:39 -0700

    THIS doesn't work:(

--- mazegen.java
+++ mazegen.java
@@ -5,75 +5,121 @@ import java.awt.geom.*;
 
 public class mazegen extends JFrame{
 
+  private cell[][] allcells;
+  private int counteri;
+  private int counterj;
+
   public mazegen(){
     setSize(850,850);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setLocationRelativeTo(null);
+
+    counteri = 0;
+    counterj = 0;
+
+    allcells = new cell[26][26];
+    for (int i = 40; i < 800 && counteri < 26; i += 30){
+      counterj = 0;
+      for (int j = 40; j < 800 && counterj < 26; j += 30){
+        allcells[counterj][counteri] = new cell(i,j);
+        counterj++;
+      }
+      counteri++;
+    }
+
   }
+
+
 
   void lines(Graphics m){
     Graphics2D g2d = (Graphics2D)m;
 
-    outer: for (int i = 30; i < 770; i += 30){
-      for (int j = 30; j < 770; j += 30){
-        g2d.fillRect(i, j, 20, 20);
+    outer: for (int i = 30; i < 800; i += 30){
+      for (int j = 30; j < 800; j += 30){
+        g2d.fillRect(i, j+10, 10, 20);
+        g2d.fillRect(i + 10, j, 20, 10);
+        g2d.fillRect(i, j, 10, 10);
+
+        //g2d.fillRect(40, 40, 20, 20);
         //create array of coordinates
       }
     }
 
-    outer: for (int i = 30; i < 780; i += 30){
-      for (int j = 50; j < 780; j += 30){
-        double q  = (double)Math.random();
-        //System.out.println(q);
-        if (q < 0.4){
-          g2d.fillRect(i, j, 20, 10);
-        }
-        else if (q < 0.5){
-          g2d.fillRect(i + 20, j - 20, 20, 10);
-          j += 30;
-        }
-        else if (q < 0.9){
-          g2d.fillRect(i + 30, j, 20, 10);
-          i += 30;
-        }
-        else{
-          g2d.fillRect(i + 30, j + 30, 20, 10);
-          i += 30;
-          j += 30;
+
+    gen(new cell(), g2d);
+
+
+
+    //g2d.fillRect(40, 40, 20, 20);
+
+}
+
+    //gen(current);
+
+
+  void gen(cell now, Graphics thing){
+
+      int x = now.getX();
+      int y = now.getY();
+
+      int xind = now.getX()/30-1;
+      int yind = now.getY()/30-1;
+
+
+      ArrayList<cell> unvisited = new ArrayList<cell>();
+      if (yind > 0){
+        if (allcells[xind][yind-1].getMarkStatus() == false){
+          unvisited.add(allcells[xind][yind-1]);
+          allcells[xind][yind-1].setDirection("left");
         }
       }
-    }
-
-    outer: for (int i = 50; i < 780; i += 30){
-      for (int j = 30; j < 780; j += 30){
-        double q  = (double)Math.random();
-        //System.out.println(q);
-        if (q < 0.25){
-          g2d.fillRect(i, j, 10, 20);
-        }
-        else if (q < 0.5){
-          g2d.fillRect(i, j+30, 10, 20);
-          j += 30;
-        }
-        else if (q < 0.75){
-          g2d.fillRect(i + 30, j, 10, 20);
-          i += 30;
-        }
-        else{
-          g2d.fillRect(i + 30, j + 30, 10, 20);
-          i += 30;
-          j += 30;
+      if (xind > 0){
+        if (allcells[xind-1][yind].getMarkStatus() == false){
+          unvisited.add(allcells[xind-1][yind]);
+          allcells[xind-1][yind].setDirection("top");
         }
       }
+      if (yind < 26){
+        if (allcells[xind][yind+1].getMarkStatus() == false){
+          unvisited.add(allcells[xind][yind+1]);
+          allcells[xind][yind+1].setDirection("right");
+        }
+      }
+      if (xind < 26){
+        if (allcells[xind+1][yind].getMarkStatus()== false){
+          unvisited.add(allcells[xind+1][yind]);
+          allcells[xind+1][yind].setDirection("bottom");
+        }
+      }
+
+      int q = (int)(Math.random() * unvisited.size());
+      System.out.println(q);
+
+      unvisited.get(q).visited(); //marked as visited
+      //unvisited.get(q).clearWall(); //clears wall
+
+      if (unvisited.get(q).getDirection() == "top"){
+        thing.clearRect(x, y-10, 20, 10);
+        System.out.println("top cleared");
+      }
+      else if (unvisited.get(q).getDirection() == "right"){
+        thing.clearRect(x+20, y, 10, 20);
+        System.out.println("right cleared");
+      }
+      else if (unvisited.get(q).getDirection() == "bottom"){
+        thing.clearRect(x, y+20, 20, 10);
+        System.out.println("bottom cleared");
+      }
+      else{
+        thing.clearRect(x-10, y, 10, 20);
+        System.out.println("left cleared");
+      }
+      System.out.println(unvisited.get(q));
+      gen(unvisited.get(q), thing);
+
+
     }
 
-
-    //g2d.drawLine(120, 200, 120, 200);
-    //g2d.drawLine(120, 50, 120, 200);
-
-    //g2d.draw(new Line2D.Double(59.2d, 99.8d, 419.1d, 99.8d));
-    //g2d.draw(new Line2D.Float(21.50f, 132.50f, 459.50f, 132.50f));
-  }
 
   public void paint(Graphics m){
     super.paint(m);
