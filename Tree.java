4fc5a3c25c170e5390d1086ace0cc1deff2c93d1
and1zhao <awzbball@gmail.com>
2018-06-02 23:59:44 -0700

    fixing transition from alive to dead

--- Tree.java
+++ Tree.java
@@ -1,9 +1,15 @@
+//import javax.swing.Timer;
+import java.util.Timer;
+
 public class Tree extends Sprite {
 
     private final int BOARD_WIDTH = 400;
+    private final int BOARD_HEIGHT = 300;
     private final int TREE_SPEED = 1;
+    protected int dx, dy;
     private boolean treeAlive = true;
     private final double direction = Math.random();
+    private double nDirection = Math.random();
 
     public Tree(int x, int y) {
         super(x, y);
@@ -15,20 +21,96 @@ public class Tree extends Sprite {
         getImageDimensions();
     }
 
+    public int getdx(){
+      return dx;
+    }
+
+    public int getdy(){
+      return dy;
+    }
+
     public void move() {
 
-        if(direction < 0.25 && direction > 0)
-          x += TREE_SPEED;
-        else if (direction > 0.25 && direction < 0.5)
-          x -= TREE_SPEED;
-        else if (direction > 0.5 && direction < 0.75)
-          y += TREE_SPEED;
-        else if (direction > 0.75 && direction < 1)
-          y -= TREE_SPEED;
+        x += dx;
+        y += dy;
 
-        if (x > BOARD_WIDTH)
-            visible = false;
+        if(direction < 0.25)
+            dx = -1;
+        else if (direction > 0.25 && direction < 0.5)
+            dx = 1;
+        else if (direction > 0.5 && direction < 0.75)
+            dy = -1;
+        else if (direction > 0.75)
+            dy = 1;
+
+        //below is my failed attempt to fix movement
+        /*if (x <= 0){
+            if (dx != 0){
+                dx = 0;
+                if(nDirection < 0.5){
+                  dy = -1;
+                } else {
+                  dy = 1;
+                }
+                /*if (direction < 0.125){
+                    dy = -1;
+                }
+                if (direction > 0.125){
+                    dy = 1;
+                }
+            }
+        }
+        if (x >= BOARD_WIDTH - width){
+            if (dx != 0){
+                dx = 0;
+                if(nDirection < 0.5){
+                  dy = -1;
+                } else {
+                  dy = 1;
+                }
+                /*if (direction < 0.375){
+                    dy = -1;
+                }
+                if (direction > 0.375){
+                    dy = 1;
+                }
+            }
+        }
+        if (y <= 0){
+            if (dy != 0){
+                dy = 0;
+                if(nDirection < 0.5){
+                  dx = -1;
+                } else {
+                  dx = 1;
+                }
+                if (direction < 0.625){
+                    dx = -1;
+                }
+                if (direction > 0.625){
+                    dx = 1;
+                }
+            }
+        }
+        if (y >= BOARD_HEIGHT - 2 * height){
+            if (dy != 0){
+                dy = 0;
+                if(nDirection < 0.5){
+                  dx = -1;
+                } else {
+                  dx = 1;
+                }
+                if (direction < 0.875){
+                    dx = -1;
+                }
+                if (direction > 0.875){
+                    dx = 1;
+                }
+            }
+        }*/
+
     }
+
 
     public boolean isAlive(){
         return treeAlive;
@@ -36,6 +118,18 @@ public class Tree extends Sprite {
 
     public void killTree(){
         treeAlive = false;
+        dx = 0;
+        dy = 0;
+        System.out.println("dx = " + dx + " dy = " + dy);
+        new java.util.Timer().schedule(
+          new java.util.TimerTask() {
+              @Override
+              public void run() {
+                  dx = 1;
+              }
+          },
+          5000
+        );
     }
 
 }
