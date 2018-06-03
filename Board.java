0c227bcbe7c65ae5fc38a116e481ca360aca9b37
and1zhao <awzbball@gmail.com>
2018-06-02 20:09:34 -0700

    governs behavior of all objects

--- Board.java
+++ Board.java
@@ -1,79 +1,193 @@
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Toolkit;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.List;
+import java.util.ArrayList;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class Board extends JPanel implements ActionListener {
 
     private Timer timer;
-    private Person person1;
+    private Person person;
+    private List<Tree> trees = new ArrayList<>();
+    private boolean inGame;
+    private final int P_X = 20;
+    private final int P_Y = 20;
+    private final int B_WIDTH = 400;
+    private final int B_HEIGHT = 300;
     private final int DELAY = 10;
+    private int maxTrees = 5;
+    private int[][] pos = new int[maxTrees][2];
 
     public Board() {
-
         initBoard();
     }
 
     private void initBoard() {
 
+        initTrees();
+
         addKeyListener(new TAdapter());
         setFocusable(true);
         setBackground(Color.black);
         setDoubleBuffered(true);
+        inGame = true;
 
-        person1 = new Person();
+        setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+
+        person = new Person(P_X, P_Y);
 
         timer = new Timer(DELAY, this);
         timer.start();
+    }
+
+    public void initTrees() {
+        setTrees();
+        System.out.println(trees);
+    }
+
+    private void setTrees(){
+      for (int[] p : pos){
+          p[0] = (int)(Math.random() * (B_WIDTH - 50)) + 50;
+          p[1] = (int)(Math.random() * (B_HEIGHT - 50)) + 50;
+          trees.add(new Tree(p[0], p[1]));
+      }
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        doDrawing(g);
+        if (inGame)
+          drawObjects(g);
+        else
+          drawGameOver(g);
 
         Toolkit.getDefaultToolkit().sync();
     }
 
-    private void doDrawing(Graphics g) {
+    private void drawObjects(Graphics g) {
 
         Graphics2D g2d = (Graphics2D) g;
 
-        g2d.drawImage(person1.getImage(), person1.getX(),
-            person1.getY(), this);
+        g2d.drawImage(person.getImage(), person.getX(), person.getY(), this);
+
+        //List<Tree> ts = person.getTrees();
+
+        if (trees.size() > 0){
+            for (Tree tree : trees) {
+                g2d.drawImage(tree.getImage(), tree.getX(), tree.getY(), this);
+            }
+        }
+
+        g2d.setColor(Color.WHITE);
+        g2d.drawString("Trees left: " + trees.size(), 5, 15);
+
+    }
+
+    private void drawGameOver(Graphics g) {
+
+        String msg = "Game Over";
+        Font small = new Font("Helvetica", Font.BOLD, 14);
+        FontMetrics fm = getFontMetrics(small);
+
+        g.setColor(Color.white);
+        g.setFont(small);
+        g.drawString(msg, (B_WIDTH - fm.stringWidth(msg)) / 2,
+                B_HEIGHT / 2);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
-        step();
+        inGame();
+
+        updatePerson();
+        updateTrees();
+
+        checkCollisions();
+
+        repaint();
     }
 
-    private void step() {
+    private void inGame() {
+        if (!inGame)
+            timer.stop();
+    }
 
-        person1.move();
+    private void updatePerson() {
+        if (person.isVisible())
+            person.move();
+    }
 
-        repaint(person1.getX()-1, person1.getY()-1,
-                person1.getWidth()+2, person1.getHeight()+2);
+    private void updateTrees() {
+
+        //List<Tree> ts = trees;//person.getTrees();
+
+        for (int i = 0; i < trees.size(); i++) {
+
+            Tree t = trees.get(i);
+
+            if (t.isVisible()) {
+                t.move();
+            } else {
+                trees.remove(i);
+            }
+        }
+    }
+
+    /*private void step() {
+
+        person.move();
+
+        repaint(person.getX()-1, person.getY()-1,
+                person.getWidth()+2, person.getHeight()+2);
+    }*/
+
+    public void checkCollisions() {
+
+        Rectangle r2 = person.getBounds();
+
+        //List<Tree> ts = trees;//person.getTrees();
+
+        for (Tree t : trees) {
+
+            Rectangle r1 = t.getBounds();
+
+                if (r1.intersects(r2)) {
+
+                    if(t.isAlive()){
+                        t.killTree();
+                        System.out.println("tree is dead");
+                    } else {
+                        System.out.println("u r ded");
+                        inGame = false;
+                    }
+
+            }
+        }
     }
 
     private class TAdapter extends KeyAdapter {
 
         @Override
         public void keyReleased(KeyEvent e) {
-            person1.keyReleased(e);
+            person.keyReleased(e);
         }
 
         @Override
         public void keyPressed(KeyEvent e) {
-            person1.keyPressed(e);
+            person.keyPressed(e);
         }
     }
 }
